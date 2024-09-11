5fdd05a4807b94934d77d204132a5cac8add3756
lstarmal <hjw3497@gmail.com>
2024-09-11 15:45:09 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,19 @@
 def print_hello():
 	print("Hello World")
+
 def print_KIT():
 	print("Kumoh National Institute of Technology.")
+
+def add(x, y):
+	a = x + y
+	return a
+
+def sq(f):
+	b = f*f
+	return b
+
 if __name__ == '__main__':
 	print_KIT()
 	print_hello()
+	print(add(3,4))
+	print(sq(6))
